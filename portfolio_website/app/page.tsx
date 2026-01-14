6b411874bd07b1434f9877ee7e5d01fb335f1787
JengCharat <zazajeng401@gmail.com>
2026-01-14 14:16:27 +0700

    about me (not decorate)

--- portfolio_website/app/page.tsx
+++ portfolio_website/app/page.tsx
@@ -4,20 +4,24 @@ export default function Home(){
     return (
         <>
             <Navbar/>
-            <div className="flex justify-center m-5">
-
-             <Image className="rounded-full"
-                  src="/images/my-profile-img.jpg"
-                  alt="profile image"
-                  width={200}
-                  height={200}
-            />  
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <h3 className="text-white">Hello I'm</h3>
-              <h1 className="text-white text-3xl font-bold">
-                Kaewpetcharat Sisan
-              </h1>
+            <div className="flex flex-col flex-1 md:flex-row md:items-start justify-center items-center m-5">
+                <div>
+                     <Image className="relative rounded-full md:w-100"
+                          src="/images/my-profile-img.jpg"
+                          alt="profile image"
+                          width={200}
+                          height={200}
+                    />  
+                </div> 
+                <div className="flex flex-col flex-1  items-center m-5 text-center md:items-start md:text-start">
+                  <h3 className="text-white">Hello I'm</h3>
+                  <h1 className="text-white text-3xl font-bold">
+                    Kaewpetcharat Sisan
+                  </h1>
+                  <h1 className="text-white text-xl">
+                    นักศึกษาชั้นปีที่ 3 สาขาวิทยาลัยการคอมพิวเตอร์คอมพิวเตอร์ สามขาวิทยาการคอมพิวเตอร์ หลงใหลในการสร้างสรรค์เทคโนโลยี มีประสบการณ์แข่งขันหุ่นยนต์ตั้งแต่มัธยม และพัฒนาโปรเจคหลากหลาย
+                  </h1>
+                </div>
             </div>
         </>
     )
