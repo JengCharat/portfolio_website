b33a8d9f3a836adf85cfef1d05449c33c856477f
JengCharat <zazajeng401@gmail.com>
2026-01-14 12:23:45 +0700

    profile Image
    backgroud color
    profile image
    littlebit title

--- portfolio_website/app/page.tsx
+++ portfolio_website/app/page.tsx
@@ -1,12 +1,24 @@
 import Navbar from "./components/Navbar"
+import Image from "next/image"
 export default function Home(){
     return (
         <>
             <Navbar/>
-            <h1 className="md:text-amber-300 lg:text-blue-300">this is responesive test</h1>
-            
+            <div className="flex justify-center m-5">
 
-            
+             <Image className="rounded-full"
+                  src="/images/my-profile-img.jpg"
+                  alt="profile image"
+                  width={200}
+                  height={200}
+            />  
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <h3 className="text-white">Hello I'm</h3>
+              <h1 className="text-white text-3xl font-bold">
+                Kaewpetcharat Sisan
+              </h1>
+            </div>
         </>
     )
 }
